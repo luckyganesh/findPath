17924757e9657607de98183ee9ef042a217a599d
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-10 19:25:48 +0530

    Introduces findNeighbours function, added test.
    * findNeighbours function gives neighbour co-cordinates for given position

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,12 @@
+const findNeighbours = function({row,column}){
+    return [{row:row+1,column},{row:row-1,column},{row,column:column+1},{row,column:column-1}];
+}
+
 const generatePath = function(length) {
     return length;
 }
 
 module.exports = {
-generatePath
+  generatePath,
+  findNeighbours
 }
--- test/libTest.js
+++ test/libTest.js
@@ -1,9 +1,27 @@
-const {generatePath} = require('../src/lib.js');
+const { generatePath , findNeighbours } = require('../src/lib.js');
 
 const { deepEqual } = require('assert');
 
 describe('generatePath', function() {
     it('should return the length of grid', function() {
         deepEqual(generatePath(2),2)
+    });
+
+});
+
+describe('findNeighbours', function() {
+    it('should return neighbours', function() {
+        let expectedOutput = [{row:2,column:1},{row:0,column:1},{row:1,column:2},{row:1,column:0}];
+        deepEqual(findNeighbours({row:1,column:1}),expectedOutput)
+    });
+
+    it('should return neighbours co-ordinates for [0,0] ', function() {
+        let expectedOutput = [{row:1,column:0},{row:-1,column:0},{row:0,column:1},{row:0,column:-1}];
+        deepEqual(findNeighbours({row:0,column:0}),expectedOutput)
+    });
+
+    it('should return neighbours co-ordinates for [-1,0] ', function() {
+        let expectedOutput = [{row:0,column:0},{row:-2,column:0},{row:-1,column:1},{row:-1,column:-1}];
+        deepEqual(findNeighbours({row:-1,column:0}),expectedOutput)
     });
 });
