6a2bbe43ff1a3642751164ec91889229ceb4cc2b
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2018-12-11 15:00:23 +0530

    Introduces main function in findPath.js

--- findPath.js
+++ findPath.js
@@ -1,7 +1,10 @@
 const { generatePath } = require('./src/lib.js');
 
+const randomGenerator = function(length) {
+    return Math.random()*length;
+}
 const main = function(){
-  console.log(generatePath(+process.argv[2]))
+  console.log(generatePath(+process.argv[2],randomGenerator))
   return ;
 }
 main();
